b02f3515d96cf0cc0fc0253965f23af6251f494b
AOB-Creator <alpamisomirbekov630@gmail.com>
2021-08-27 16:09:10 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+    document.querySelector(".hidden-icon i").addEventListener("click" , ()=>{
+        document.querySelector(".hidden-icon i").classList.add("anime")
+        console.log(1)
+        
+        setTimeout(()=>{
+            document.querySelector(".hidden-icon i").classList.remove("anime")
+            document.querySelector(".hidden-icon i").classList.toggle("ti-view-list")
+            document.querySelector(".hidden-icon i").classList.toggle("ti-close")
+            
+        }, 250)
+    })
+
+    setTimeout(()=>{
+        document.querySelector(".spinner").classList.add("none")
+        document.querySelector("#wifi-loader").classList.add("none")
+        document.querySelector(".mainsddsdgfgfdgdfg").classList.remove("none")
+        }, 4500)
+
+ 
+    
+    
+
